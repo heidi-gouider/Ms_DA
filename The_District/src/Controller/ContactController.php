ce9e0586b57936ed4d19aa5ceed6a6d6a6387f59
gouider heidi <gouider.heidi@gmail.com>
2023-10-26 16:53:52 +0200

    add contact form

---
+++ The_District/src/Controller/ContactController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactFormType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function index(Request $request): Response
+    {
+        $form = $this->createForm(ContactFormType::class);
+
+        return $this->render('contact/index.html.twig', [
+            'form' => $form
+        ]);
+    }
+}
---
+++ The_District/src/Form/ContactFormType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('email')
+            ->add('telephone')
+            ->add('message')
+            // ->add('jaccepte les conditions dutilisation', CheckboxType::class, ['mapped' => false])
+            ->add('save', SubmitType::class, [
+                'label' => 'Envoyer' 
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
