b278254896f9b003912f2a28419ff061752eced8
gouider heidi <gouider.heidi@gmail.com>
2023-11-08 17:02:20 +0100

    crea panier et validation commande

--- The_District/src/Controller/CommandeController.php
+++ The_District/src/Controller/CommandeController.php
@@ -2,21 +2,65 @@
 
 namespace App\Controller;
 
+use App\Entity\Commande;
+use App\Entity\Detail;
+use App\Repository\PlatRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/commande', name: 'app_commande_')]
 class CommandeController extends AbstractController
 {
     #[isGranted('ROLE_CLIENT', message: "Vous devez avoir un compte pour accèder à cette page!")]
-    #[Route('/commande', name: 'app_commande')]
 
-    public function index(): Response
+    #[Route('/ajout', name: 'ajout')]
+    public function ajout(SessionInterface $session, PlatRepository $platRepository, EntityManagerInterface $em): Response
     {
         //on restrictionne l'accès ICI :
         $this->denyAccessUnlessGranted('ROLE_CLIENT');
 
+        $panier = $session->get('panier', []);
+
+
+        if($panier === []){
+            $this->addFlash('message', 'Votre panier est vide');
+            return $this->redirectToRoute('accueil');
+        }
+  //Le panier n'est pas vide, on crée la commande
+  $commande = new Commande();
+
+  // On remplit la commande
+  $commande->setUtilisateur($this->getUser());
+
+  // On parcourt le panier pour créer les détails de commande
+  foreach($panier as $item => $quantite){
+      $Detail = new Detail();
+
+      // On va chercher le produit
+      $plat = $platRepository->find($item);
+      
+      $prix = $plat->getPrix();
+
+      // On crée le détail de commande
+      $Detail->setPlat($plat);
+    //   $Detail->setTotal($prix);
+      $Detail->setQuantite($quantite);
+
+      $commande->addDetail($Detail);
+  }
+
+  // On persiste et on flush
+  $em->persist($commande);
+  $em->flush();
+
+  $session->remove('panier');
+
+  $this->addFlash('message', 'Commande créée avec succès');
+  return $this->redirectToRoute('accueil');
         return $this->render('commande/index.html.twig', [
             'controller_name' => 'CommandeController',
         ]);
--- The_District/src/Controller/PanierController.php
+++ The_District/src/Controller/PanierController.php
@@ -3,24 +3,45 @@
 namespace App\Controller;
 
 use App\Entity\Plat;
+use App\Repository\PlatRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-// use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType; 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/panier', name: 'panier_')]
 class PanierController extends AbstractController
 {
+    #[Route('/', name: 'index')]
+    public function index(SessionInterface $session, PlatRepository $platRepo)
+    {
+        $panier = $session->get('panier', []);
+
+        $data = [];
+        $total = 0;
+
+        foreach ($panier as $id => $quantite) {
+            $plat = $platRepo->find($id);
+            $data[] = [
+                'plat' => $plat,
+                'quantite' => $quantite
+            ];
+            $total += $plat->getPrix() * $quantite;
+        }
+        return $this->render('panier/index.html.twig', compact('data', 'total'));
+        // dd($data);
+    }
+
     #[Route('/ajouter/{id}', name: 'ajouter')]
     public function add(Plat $plat, SessionInterface $session, Request $request)
     {
 
-        $quantite = $request->request->get('quantite'); 
+        $quantite = $request->request->get('quantite');
         $action = $request->request->get('action');
 
-                // Effectue la logique de gestion de quantité
+        // Effectue la logique de gestion de quantité
         // if ($quantite < 1) {
         //     $quantite = 1;
         // }        
@@ -33,17 +54,20 @@ class PanierController extends AbstractController
 
 
         $form = $this->createFormBuilder()
-        ->add('quantite', IntegerType::class, [
-            'attr' => ['min' => 1, 'max' => 10], // Définis les valeurs minimales et maximales
-        ])
-        ->getForm();
+            ->add('quantite', IntegerType::class, [
+                'attr' => ['min' => 1, 'max' => 10],
+                // Définis les valeurs minimales et maximales
+            ])
+            ->getForm();
 
-    $form->handleRequest($request);
+        $form->handleRequest($request);
 
-    if ($form->isSubmitted() && $form->isValid()) {
-        $quantite = $form->get('quantite')->getData();
-        // Traitement des données, y compris la quantité choisie
-    }
+        if ($form->isSubmitted() && $form->isValid()) {
+            $quantite = $form->get('quantite')->getData();
+            // Traitement des données, y compris la quantité choisie
+        }
+        // récuerer l'id du plat
+        // $id = $plat->getId();
         // Obtenez le panier existant à partir de la session ou créez-en un nouveau.
         $panier = $session->get('panier', []);
 
@@ -53,14 +77,78 @@ class PanierController extends AbstractController
         // Enregistrez le panier mis à jour dans la session.
         $session->set('panier', $panier);
 
-        // Redirigez l'utilisateur vers une autre page ou effectuez toute autre action nécessaire.
-        // Par exemple, redirigez l'utilisateur vers la page du panier.
 
-        dd($session);
-        // return $this->redirectToRoute('app_commande');
+         // Retournez une réponse, mais pas de redirection.
+        //  return new Response('La quantité a été mise à jour dans le panier.');
+        // dd($session);
 
+        // return $this->redirectToRoute('panier_index');
+
+ // Récupérez l'URL de la page actuelle et stockez-la dans la session.
+        $referer = $request->headers->get('referer');
+        $session->set('referer_url', $referer);
+
+        // Redirigez l'utilisateur vers la page précédente.
+        return $this->redirect($referer);        
         // return $this->render('panier/index.html.twig', [
         //     'controller_name' => 'PanierController',
         // ]);
     }
+
+    #[Route('/ajout/{id}', name: 'ajout')]
+    public function ajout(Plat $plat, SessionInterface $session)
+    {
+        $id = $plat->getId();
+        $panier = $session->get('panier', []);
+
+        if (empty($panier[$id])) {
+            $panier[$id] = 1;
+        } else {
+            $panier[$id]++;
+        }
+        
+        $session->set('panier', $panier);
+
+
+        return $this->redirectToRoute('panier_index');
+    }
+
+    #[Route('/retirer/{id}', name: 'retirer')]
+    public function retirer(Plat $plat, SessionInterface $session)
+    {
+
+        // récuerer l'id du plat
+        $id = $plat->getId();
+        // Obtenez le panier existant à partir de la session ou créez-en un nouveau.
+        $panier = $session->get('panier', []);
+
+        if (!empty($panier[$id])) {
+            if ($panier[$id] > 1)
+                $panier[$id]--;
+        } else {
+            unset($panier[$id]);
+        }
+        // Enregistrez le panier mis à jour dans la session.
+        $session->set('panier', $panier);
+
+
+        return $this->redirectToRoute('panier_index');
+    }
+
+    #[Route('/supprimer/{id}', name: 'supprimer')]
+    public function supprimer(Plat $plat, SessionInterface $session)
+    {
+        $id = $plat->getId();
+        $panier = $session->get('panier', []);
+
+        if (!empty($panier[$id])) {
+            unset($panier[$id]);
+        }
+        $session->set('panier', $panier);
+
+
+        return $this->redirectToRoute('panier_index');
+    }
+
+
 }
